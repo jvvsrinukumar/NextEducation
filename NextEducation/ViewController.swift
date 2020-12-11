edab7dd9c0c8b7855a6884bd67e2d2bc40998bf5
V V Srinu Kumar Jonnalagadda <jvvsrinukumar@gmail.com>
2020-12-11 16:53:04 +0530

    Initial Commit

---
+++ NextEducation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NextEducation
+//
+//  Created by V V Srinu Kumar Jonnalagadda on 11/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
